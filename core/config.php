b591c05d94f5543d7eeed23b2c7db7a7005ef740
Meet patel <patelmeet0037@gmail.com>
2021-05-05 22:49:27 +0530

    Update config.php

--- core/config.php
+++ core/config.php
@@ -7,8 +7,8 @@
 
 	//SMTP
 	define("M_HOST", 'smtp.gmail.com');
-	define("M_USERNAME", 'patelmeet0037@gmail.com');
-	define("M_PASSWORD", 'Meet0037');
+	define("M_USERNAME", 'youremail');
+	define("M_PASSWORD", 'yourpassword');
 	define("M_SMTPSECURE", 'ssl');
 	define("M_PORT", "465");
 	
